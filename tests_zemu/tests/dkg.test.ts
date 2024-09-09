e1769ca57218d50ff3a57cceefa4f46069e6e715
emmanuelm41 <emmanuelm41@gmail.com>
2024-09-09 13:24:57 -0300

    feat: change dkg tests to 3 participants

--- tests_zemu/tests/dkg.test.ts
+++ tests_zemu/tests/dkg.test.ts
@@ -47,7 +47,7 @@ describe.each(models)('DKG', function (m) {
         }
     })
 
-    describe.each([{p:2, min:2}])('participants', function ({p: participants, min: minSigners}){
+    describe.each([{p:3, min:2}])('participants', function ({p: participants, min: minSigners}){
         it("p: " + participants + " - min: " + minSigners, async function(){
             const checkSimRequired = (sims: Zemu[], i:number): {sim: Zemu, created:boolean} => {
                 let created = false;
